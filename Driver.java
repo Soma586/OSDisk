c0257c8efb7e79eb7d8e0c4b047414919bca1636
Tarik <lightspeedkey@yahoo.com>
2019-12-10 17:07:23 -0500

    added a main class to call the other classes

---
+++ Driver.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		FCFS fc = new FCFS();
+		SSTF st = new SSTF();
+		SCAN sc = new SCAN();
+		
+
+	}
+
+}
